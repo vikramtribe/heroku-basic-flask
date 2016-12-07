1541ea29332a9e06abec5983c66dc5b55dc8f617
Vikram <vikramtribe@gmail.com>
2016-12-07 16:19:49 +0800

    try la

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def homepage():
 
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def form_go():
+
+	topic = request.form['topic']
+	genre = int(request.form['genre'])
+	country = int(request.form['country'])
+	length = request.form['length']
+
+	return (topic, genre)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
