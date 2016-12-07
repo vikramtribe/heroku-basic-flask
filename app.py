7947451f4f779a40dc08b03379b72784b796f652
Vikram <vikramtribe@gmail.com>
2016-12-07 15:04:14 +0800

    start

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+
+    return "rushmore"
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
