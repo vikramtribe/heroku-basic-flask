be4933b3a3dd28979a931eb6c142065361a6b24e
Vikram <vikramtribe@gmail.com>
2016-12-07 15:18:59 +0800

    render homepage

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
 
-    return "rushmore"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
